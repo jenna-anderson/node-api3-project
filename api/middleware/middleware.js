9d2308f52d9edbd01a4272eb5d65c697bccfdeb1
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-07 14:41:07 -0600

    wires index.js, api/server.js, users/users-router.js, and middleware/middleware.js together with skeleton structure

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,31 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log('very nice')
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('user id validated')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('user is validated')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('post is validated')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -5,6 +5,12 @@ const server = express();
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+server.use(express.json());
+
+const usersRouter = require('./users/users-router');
+const { logger } = require('./middleware/middleware');
+
+server.use('/api/users', logger, usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
